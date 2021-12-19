554bf889a3bce70db958f2bade46ec0846e02bba
Metehan Murad Tali <metehanmuradtali@gmail.com>
2021-12-19 19:30:56 +0300

    Added some bootstrap and other components

--- post/models.py
+++ post/models.py
@@ -1,9 +1,23 @@
 from django.db import models
+from django.urls import reverse
+
 
 # Create your models here.
 class Post(models.Model):
-    title = models.CharField(max_length=120,verbose_name="Başlık")
-    file= models.FileField(null=True,blank=True)
+    title = models.CharField(max_length=120, verbose_name="Başlık")
+    file = models.FileField(null=True, blank=True)
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('post:detail', kwargs={'id': self.id})
+
+    def get_update_url(self):
+        return reverse('post:update', kwargs={'id': self.id})
+
+    def get_delete_url(self):
+        return reverse('post:delete', kwargs={'id': self.id})
+
+    def get_create_url(self):
+        return reverse('post:create')
--- post/views.py
+++ post/views.py
@@ -1,4 +1,48 @@
-from django.shortcuts import render, HttpResponse
-
+from django.shortcuts import render, get_object_or_404,HttpResponseRedirect,redirect
+from .models import Post
+from .forms import PostForm
+from django.contrib import messages
 # Create your views here.
+
+def post_index(request):
+    posts = Post.objects.all()
+    return render(request, 'post/index.html', {'posts': posts})
+
+
+def post_detail(request,id):
+    post = get_object_or_404(Post, id=id)
+    return render(request, 'post/detail.html', {'post': post})
+
+
+def post_create(request):
+    form = PostForm(request.POST or None,request.FILES or None)
+    if form.is_valid():
+       post = form.save()
+       messages.success(request,'Post başarılı şekilde oluşturuldu')
+       return HttpResponseRedirect(post.get_absolute_url())
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'post/form.html', context)
+
+def post_update(request,id):
+    post = get_object_or_404(Post, id=id)
+    form = PostForm(request.POST or None, request.FILES or None,instance=post)
+    if form.is_valid():
+       form.save()
+       messages.success(request,'Post başarılı şekilde güncellendi')
+       return HttpResponseRedirect(post.get_absolute_url())
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'post/form.html', context)
+
+def post_delete(request,id):
+    post = get_object_or_404(Post, id=id)
+    post.delete()
+    messages.success(request, 'Post başarılı şekilde silindi',extra_tags='mesaj-basarili')
+    return redirect('post:index')
+
 
